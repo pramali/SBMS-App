c05d16ea986045c0b5e063af38ac0760c5e90db1
pramod.mali@gmail.com <pramod.mali@gmail.com>
2023-09-28 01:08:13 +0530

    featu-300 changes added

--- 64-SBMS-Test-App/src/main/java/com/ashokit/Application.java
+++ 64-SBMS-Test-App/src/main/java/com/ashokit/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+     Map map=new HashMap();
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
